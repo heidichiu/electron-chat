99313483de66f689a479203788d4ba0eb26bf247
heidichiu <heidi@xqtem.com>
2021-01-04 12:19:42 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Notification } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
